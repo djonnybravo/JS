ce1c6055fa91479ab8ba8e4452417422ab170e27
djonnybravo <djonnybravo29@gmail.com>
2022-09-10 15:14:56 +0300

    Multiply

--- codewars js solutions/script.js
+++ codewars js solutions/script.js
@@ -1,6 +1,3 @@
-function boolToWord( bool ){
-    if (bool == true) {
-      return str = 'Yes'
-    } else if (bool == false) {
-      return str = 'No'
-    }
+function multiply(a, b){
+    return a * b;
+   }
