fa38971100d63fce3258b402d3212046b917fcd1
djonnybravo <djonnybravo29@gmail.com>
2022-09-13 13:34:24 +0300

    String repeat

--- codewars js solutions/script.js
+++ codewars js solutions/script.js
@@ -1,16 +1,5 @@
-function getGrade (s1, s2, s3) {
-    score = (s1 + s2 + s3) / 3;
-    
-    if(score>=90 && score <= 100){
-      return 'A'    
-    }else if(score>=80 && score < 90){
-      return 'B'
-    }else if (score>=70 && score < 80){
-      return 'C'
-    }else if (score>=60 && score < 70){
-      return 'D'
-    }else if (score>=0 && score < 60){
-      return 'F'
-    }
-    }
-  
+function repeatStr (n, s) {
+  return s.repeat(n)
+}
+
+console.log(repeatStr(4, "d"))
