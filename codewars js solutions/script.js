9d474d0ba45f5ec272cc5980e2946d701795c3b2
djonnybravo <djonnybravo29@gmail.com>
2022-09-16 16:35:32 +0300

    Jaden Casing Strings

--- codewars js solutions/script.js
+++ codewars js solutions/script.js
@@ -1,3 +1,10 @@
-function opposite(number) {
-  return (-number);
-}
+String.prototype.toJadenCase = function () {
+ 
+  var nStr = "";
+  
+  this.split(" ").forEach(function(s) { 
+      nStr = nStr + " " + s.substring(0,1).toUpperCase() + s.substring(1); 
+  });
+
+  return nStr.substr(1);
+};
