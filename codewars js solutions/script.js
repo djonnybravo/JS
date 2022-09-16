092b6dc553330972320d36e8ab5d3d3a39a5a048
djonnybravo <djonnybravo29@gmail.com>
2022-09-16 16:31:46 +0300

    Remove an exclamation mark from the end of string

--- codewars js solutions/script.js
+++ codewars js solutions/script.js
@@ -1,29 +1,6 @@
-const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
-
-function golfScore(par, strokes) {
-  
-    if (strokes == 1) {
-        return names[0];
-    }else if (strokes <= par - 2){
-        return names[1];
-    }else if (strokes == par - 1){
-        return names[2];
-    }else if (strokes == par) {
-        return names[3];
-    }else if (strokes == par + 1){
-        return names[4];
-    }else if (strokes == par + 2){
-        return names[5];
-    }else if (strokes >= par + 3){
-        return names[6];
-    }
-
-
-  return "Change Me";
-} 
-
-console.log(golfScore(5, 5));
-
-
-
-
+function remove (string) {
+  if (string.endsWith("!")) {
+    return string.substring(0, string.length - 1);
+  }
+  return string;
+}
