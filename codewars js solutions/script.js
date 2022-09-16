96b4483c47611361b6f7ac588627796791c0daea
djonnybravo <djonnybravo29@gmail.com>
2022-09-16 16:35:49 +0300

    Function 1 - hello world

--- codewars js solutions/script.js
+++ codewars js solutions/script.js
@@ -1,10 +1,3 @@
-String.prototype.toJadenCase = function () {
- 
-  var nStr = "";
-  
-  this.split(" ").forEach(function(s) { 
-      nStr = nStr + " " + s.substring(0,1).toUpperCase() + s.substring(1); 
-  });
-
-  return nStr.substr(1);
-};
+function greet() {
+  return "hello world!";
+}
